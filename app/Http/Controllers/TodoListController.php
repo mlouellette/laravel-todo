b29b1a202e01f93eab8f6cc62ba894e6e8645dc1
mlouellette <112026857+mlouellette@users.noreply.github.com>
2023-07-18 21:55:41 -0400

    commit

---
+++ app/Http/Controllers/TodoListController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ListItem;
+
+class TodoListController extends Controller
+{
+    public function index() {
+        return view('welcome', ['listItems' => ListItem::where('is_complete', 0)->get()]);
+    }
+
+    public function markComplete($id) {
+        $listItem = ListItem::find($id);
+        $listItem->is_complete = 1;
+        $listItem->save();
+
+        return redirect('/');
+    }
+
+    public function saveItem(Request $request) {
+        // \Log::info(json_encode($request->all()));
+
+        $newListItem = new ListItem;
+        $newListItem->name = $request->listItem;
+        $newListItem->is_complete = 0;
+        $newListItem->save();
+
+        return redirect('/');
+    }
+}
